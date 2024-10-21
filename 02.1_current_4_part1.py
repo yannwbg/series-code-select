0330de90796d626f3a7ff68fe420faafa1874ad3
shendjlingsu <shendjlingsu@gmail.com>
2024-10-21 15:32:23 -0500

    Update 02.1_current_4_part1.py
    For un4_current datafile
    -add n_sectors
    -discard those missing sectors (unless miss only D or E)
    -export also the discarded rows to a sepreate file
    -change csv file names

--- 02.1_current_4_part1.py
+++ 02.1_current_4_part1.py
@@ -3,10 +3,16 @@
 #print(current_path)
 
 import pandas as pd
+import numpy as np
 pd.set_option('display.max_columns', None)
 
 df_current_4 = pd.read_csv("/Users/Danjing 1/Lingsu/Jobs/2024 WB STC/Sector/Data_original/cleaned/un4_current.csv")
 print(df_current_4.head())
+
+# Get the ios and country columns and turn into a mapping file.
+iso_mapping = df_current_4.iloc[:, :2].drop_duplicates()
+#print(iso_mapping.head())
+iso_mapping.to_csv("/Users/Danjing 1/Lingsu/Jobs/2024 WB STC/Sector/Process/iso_mapping.csv", index=False)
 
 ##########Step 2. Sector check
 ##### Transforme the original dataset
@@ -38,15 +44,25 @@ def missing_column(row):
 
 pivot_df_current_4['sector_missing']= pivot_df_current_4.loc[:, 'A':'R+S+T'].apply(missing_column, axis=1)
 
+# Add a column showing the number of sectors. If no missing sector, it should be 15.
+columns_to_cal = [3,4]+list(range(6,19))
+pivot_df_current_4['n_sectors'] = pivot_df_current_4.iloc[:, columns_to_cal].count(axis=1)
+#pivot_df_current_4['n_sectors'] = columns.apply(lambda x: x.count(), axis=1)
 
-# reset year and series into integer
-pivot_df_current_4['year'] = pivot_df_current_4['year'].astype(int)
-pivot_df_current_4['series_code'] = pivot_df_current_4['series_code'].astype(int)
+# Add a column to check if D is missing. Another column to show if E is missing. Another column to show if B is missing.
+pivot_df_current_4['B_missing'] = pivot_df_current_4['B'].isna()
+pivot_df_current_4['D_missing'] = pivot_df_current_4['D'].isna()
+pivot_df_current_4['E_missing'] = pivot_df_current_4['E'].isna()
+
+# pivot_df_current_4.info()
+pivot_df_current_4['only_B_missing'] = (pivot_df_current_4['B_missing'] == True) & (pivot_df_current_4['n_sectors'] == 14)
+pivot_df_current_4['only_DorE_missing'] = ((pivot_df_current_4['D_missing'] == True) & (pivot_df_current_4['n_sectors'] == 14)) | ((pivot_df_current_4['E_missing'] == True) & (pivot_df_current_4['n_sectors'] == 14))
 
 # print(pivot_df_current_4.head(10))
+
 # calculate the discard score
-columns_to_sum = [3,4]+list(range(6,19))
-pivot_df_current_4['sum_of_sector'] = pivot_df_current_4.iloc[:,columns_to_sum].sum(axis=1)
+#columns_to_sum = [3,4]+list(range(6,19))
+pivot_df_current_4['sum_of_sector'] = pivot_df_current_4.iloc[:,columns_to_cal].sum(axis=1)
 pivot_df_current_4['score'] = ((pivot_df_current_4['sum_of_sector'] - pivot_df_current_4['B.1g'])/pivot_df_current_4['B.1g']).abs()
 pivot_df_current_4['discard'] = (pivot_df_current_4['score']-0.005)>=0
 
@@ -55,6 +71,7 @@ for index in range(len(pivot_df_current_4)):
     if pd.isna(pivot_df_current_4.loc[index, 'B.1g']):  # Check if the value in A is NaN (or None)
         pivot_df_current_4.loc[index, 'discard'] = False     # Set B to False if A is empty
 
+print(pivot_df_current_4.head(10))
 
 ###FISIM not considered yet.
 # check if footnote 6 and footnote 36 is included.
@@ -83,24 +100,44 @@ for index in range(len(pivot_df_current_4)):
         else:
             pivot_df_current_4['discard'] = (pivot_df_current_4['score'] - 0.075) >= 0
 
+
+# Save a copy of current discard column, without considering the number of sectors.
+pivot_df_current_4['discard_nc_Nsector'] = pivot_df_current_4['discard']
+
+# If the n_sector is smaller than 15 (total sector columns), unless only missing D or E, then discard. Otherwise,  do not discard.
+conditions = (pivot_df_current_4['n_sectors'] < 15) & (pivot_df_current_4['only_DorE_missing'] == False)
+pivot_df_current_4.loc[conditions, 'discard'] = True
+
 #Our period of interest is 1990 – 2023.
 pivot_df_current_4 = pivot_df_current_4[(pivot_df_current_4['year']>=1990) & (pivot_df_current_4['year']<=2023)]
 
+# reset year and series into integer
+pivot_df_current_4['year'] = pivot_df_current_4['year'].astype(int)
+pivot_df_current_4['series_code'] = pivot_df_current_4['series_code'].astype(int)
+
+# Merge the iso3
+pivot_df_current_4 = pd.merge(pivot_df_current_4, iso_mapping, on='country', how='left')
+
 #export the pivot table dataframe to a csv file
 print(pivot_df_current_4.head(10))
-pivot_df_current_4.to_csv("/Users/Danjing 1/Lingsu/Jobs/2024 WB STC/Sector/Process/Current_4_Sectorvalue.csv")
+pivot_df_current_4.to_csv("/Users/Danjing 1/Lingsu/Jobs/2024 WB STC/Sector/Process/un4_current_Sectorvalue.csv")
 
+# Keep only those not discarded.
 df_current_4_int = pivot_df_current_4[pivot_df_current_4['discard']== False]
-df_current_4_int.drop(columns='discard', inplace=True)
+# df_current_4_int.drop(columns='discard', inplace=True)
 print(df_current_4_int.head(10))
+df_current_4_int.to_csv("/Users/Danjing 1/Lingsu/Jobs/2024 WB STC/Sector/Process/un4_current_intermediate.csv")
 
-df_current_4_int.to_csv("/Users/Danjing 1/Lingsu/Jobs/2024 WB STC/Sector/Process/Current_4_intermediate.csv")
+# Keep those discarded in a seperate file
+df_current_4_int_discard = pivot_df_current_4[pivot_df_current_4['discard']== True]
+df_current_4_int_discard.to_csv("/Users/Danjing 1/Lingsu/Jobs/2024 WB STC/Sector/Process/un4_current_intermediate_discard.csv")
 
-df_current_4_int = df_current_4_int.iloc[:,0:3]
+# Export a file that only contains country, year, series_code, and iso3.
+df_current_4_int = pd.concat([df_current_4_int.iloc[:,0:3],df_current_4_int.iloc[:,-1: ]], axis=1)
 print(df_current_4_int.head())
-df_current_4_int.to_csv("/Users/Danjing 1/Lingsu/Jobs/2024 WB STC/Sector/Process/Current_4_3cols.csv")
+df_current_4_int.to_csv("/Users/Danjing 1/Lingsu/Jobs/2024 WB STC/Sector/Process/un4_current_4cols.csv")
 
-###small issues:
+# ###small issues:
 ###1)Footnote 42 & 43 Refers to Gross Domestic Product. Philippines: For B.1g marked 42 & 43, the other sectors marked 19. However, for series 100, 2000-2012, sector D 2001-2008 marked 1, all other sectors has no footnote.
-###2) ISO3 has not been appended yet.
+
 
