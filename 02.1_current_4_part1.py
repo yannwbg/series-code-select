c200f0fabf326dde97e3145fce5f9c7ba490b6d9
shendjlingsu <shendjlingsu@gmail.com>
2024-10-23 23:12:26 -0500

    ISICC 4 files Part 1 update
    --add discard_on_sector & discard_on_score
    --Only keep columns needed for intermediate file

--- 02.1_current_4_part1.py
+++ 02.1_current_4_part1.py
@@ -9,31 +9,31 @@ pd.set_option('display.max_columns', None)
 df_current_4 = pd.read_csv("/Users/Danjing 1/Lingsu/Jobs/2024 WB STC/Sector/Data_original/cleaned/un4_current.csv")
 print(df_current_4.head())
 
-# Get the ios and country columns and turn into a mapping file.
-iso_mapping = df_current_4.iloc[:, :2].drop_duplicates()
+### Get the ios and country columns and turn into a mapping file.
+iso_mapping_un4 = df_current_4.iloc[:, :2].drop_duplicates()
 #print(iso_mapping.head())
-iso_mapping.to_csv("/Users/Danjing 1/Lingsu/Jobs/2024 WB STC/Sector/Process/iso_mapping.csv", index=False)
+iso_mapping_un4.to_csv("/Users/Danjing 1/Lingsu/Jobs/2024 WB STC/Sector/Process/iso_mapping_un4.csv", index=False)
 
-##########Step 2. Sector check
-##### Transforme the original dataset
-######## For Current file
+#######################################################################################################################
+############################### Sector check ##########################################################################
+####### Transforme the original dataset
 
-#Create a pivot table for the values, all sectors became column numbers
+### Create a pivot table for the values, all sectors became column numbers
 pivot_df_current_4 = df_current_4.pivot_table(index=['country','year','series_code'], columns='code', values = 'value')
 
-#Aggregate the footnote colum to check for any footnotes
+### Aggregate the footnote colum to check for any footnotes
 #print(df_current_4['note'].dtype) #object
 footnotes = df_current_4.groupby(['country','year','series_code'])['note'].agg(lambda x: ', '.join(set(y for y in x if pd.notna(y) and y != '')))
 
 #Reset the index to make 'country','year','series_code' into columns
 pivot_df_current_4.reset_index(inplace=True)
 
-# The footnotes series_code needs to have its index reset for joining. Then add it
-# to the pivot table.
+# The footnotes series_code needs to have its index reset for joining. Then add it to the pivot table.
 footnotes = footnotes.reset_index()
 footnotes.rename(columns={'note':'Footnotes'}, inplace=True)
 pivot_df_current_4 = pivot_df_current_4.merge(footnotes, on=['country','year','series_code'], how='left')
 
+### Missing sectors check
 # put all missing sectors into a new column
 '''Define a function to find missing sectors (columns)'''
 def missing_column(row):
@@ -41,7 +41,7 @@ def missing_column(row):
 
 pivot_df_current_4['sector_missing']= pivot_df_current_4.loc[:, 'A':'R+S+T'].apply(missing_column, axis=1)
 
-# Add a column showing the number of sectors. If no missing sector, it should be 15.
+## Add a column showing the number of sectors. If no missing sector, it should be 15.
 columns_to_cal = [3,4]+list(range(6,19))
 pivot_df_current_4['n_sectors'] = pivot_df_current_4.iloc[:, columns_to_cal].count(axis=1)
 #pivot_df_current_4['n_sectors'] = columns.apply(lambda x: x.count(), axis=1)
@@ -57,20 +57,15 @@ pivot_df_current_4['only_DorE_missing'] = ((pivot_df_current_4['D_missing'] == T
 
 # print(pivot_df_current_4.head(10))
 
-# calculate the discard score
+### calculate the discard score
 #columns_to_sum = [3,4]+list(range(6,19))
 pivot_df_current_4['sum_of_sector'] = pivot_df_current_4.iloc[:,columns_to_cal].sum(axis=1)
 pivot_df_current_4['score'] = ((pivot_df_current_4['sum_of_sector'] - pivot_df_current_4['B.1g'])/pivot_df_current_4['B.1g']).abs()
 pivot_df_current_4['discard'] = (pivot_df_current_4['score']-0.005)>=0
 
-# if b.1g is empty, do not discard. (discard = false)
-for index in range(len(pivot_df_current_4)):
-    if pd.isna(pivot_df_current_4.loc[index, 'B.1g']):  # Check if the value in A is NaN (or None)
-        pivot_df_current_4.loc[index, 'discard'] = False     # Set B to False if A is empty
-
 print(pivot_df_current_4.head(10))
 
-###FISIM not considered yet.
+### FISIM.
 # check if footnote 6 and footnote 36 is included.
 pivot_df_current_4['footnote_6'] = pivot_df_current_4['Footnotes'].str.contains(r'\b6\b', na=False)
 pivot_df_current_4['footnote_36'] = pivot_df_current_4['Footnotes'].str.contains('36', na=False)
@@ -98,14 +93,23 @@ for index in range(len(pivot_df_current_4)):
             pivot_df_current_4['discard'] = (pivot_df_current_4['score'] - 0.075) >= 0
 
 
-# Save a copy of current discard column, without considering the number of sectors.
-pivot_df_current_4['discard_nc_Nsector'] = pivot_df_current_4['discard']
+### if b.1g is empty, do not discard. (discard = false)
+for index in range(len(pivot_df_current_4)):
+    if pd.isna(pivot_df_current_4.loc[index, 'B.1g']):  # Check if the value in A is NaN (or None)
+        pivot_df_current_4.loc[index, 'discard'] = False     # Set B to False if A is empty
+
+
+### Save a copy of current discard column, only considering the scores, without considering the number of sectors.
+pivot_df_current_4['discard_on_score'] = pivot_df_current_4['discard']
 
 # If the n_sector is smaller than 15 (total sector columns), unless only missing D or E, then discard. Otherwise,  do not discard.
 conditions = (pivot_df_current_4['n_sectors'] < 15) & (pivot_df_current_4['only_DorE_missing'] == False)
+pivot_df_current_4['discard_on_sector'] = conditions
 pivot_df_current_4.loc[conditions, 'discard'] = True
 
-### Final Data Clean and Export
+#######################################################################################################################
+################# Final Data Clean and Export
+
 #Our period of interest is 1990 – 2023.
 pivot_df_current_4 = pivot_df_current_4[(pivot_df_current_4['year']>=1990) & (pivot_df_current_4['year']<=2023)]
 
@@ -114,24 +118,34 @@ pivot_df_current_4['year'] = pivot_df_current_4['year'].astype(int)
 pivot_df_current_4['series_code'] = pivot_df_current_4['series_code'].astype(int)
 
 # Merge the iso3
-pivot_df_current_4 = pd.merge(pivot_df_current_4, iso_mapping, on='country', how='left')
+pivot_df_current_4 = pd.merge(pivot_df_current_4, iso_mapping_un4, on='country', how='left')
 
-#export the pivot table dataframe to a csv file
+# export the pivot table dataframe to a csv file
 print(pivot_df_current_4.head(10))
 pivot_df_current_4.to_csv("/Users/Danjing 1/Lingsu/Jobs/2024 WB STC/Sector/Process/un4_current_Sectorvalue.csv")
 
-# Keep only those not discarded.
-df_current_4_int = pivot_df_current_4[pivot_df_current_4['discard']== False]
-# df_current_4_int.drop(columns='discard', inplace=True)
-print(df_current_4_int.head(10))
-df_current_4_int.to_csv("/Users/Danjing 1/Lingsu/Jobs/2024 WB STC/Sector/Process/un4_current_intermediate.csv")
-
 # Keep those discarded in a seperate file
 df_current_4_int_discard = pivot_df_current_4[pivot_df_current_4['discard']== True]
+# df_current_4_int.drop(columns='discard', inplace=True)
+df_current_4_int_discard.reset_index(inplace=True, drop=True)
+print(df_current_4_int_discard.tail(10))
 df_current_4_int_discard.to_csv("/Users/Danjing 1/Lingsu/Jobs/2024 WB STC/Sector/Process/un4_current_intermediate_discard.csv")
 
+# Keep only those not discarded.
+df_current_4_int = pivot_df_current_4[pivot_df_current_4['discard']== False]
+df_current_4_int.drop(columns='discard', inplace=True)
+print(df_current_4_int.tail(10))
+
+# Keep only columns interested.
+columns_to_keep = ['country', 'iso3', 'year','series_code','Footnotes','sector_missing','score','discard_on_score', 'discard_on_sector']
+df_current_4_int = df_current_4_int.loc[:, columns_to_keep]
+df_current_4_int.reset_index(inplace=True, drop=True)
+print(df_current_4_int.tail(10))
+df_current_4_int.to_csv("/Users/Danjing 1/Lingsu/Jobs/2024 WB STC/Sector/Process/un4_current_intermediate.csv")
+
+
 # Export a file that only contains country, year, series_code, and iso3.
-df_current_4_int = pd.concat([df_current_4_int.iloc[:,0:3],df_current_4_int.iloc[:,-1: ]], axis=1)
+df_current_4_int = df_current_4_int.iloc[:,0:4]
 print(df_current_4_int.head())
 df_current_4_int.to_csv("/Users/Danjing 1/Lingsu/Jobs/2024 WB STC/Sector/Process/un4_current_4cols.csv")
 
--- 02.2_constant_4_part1.py
+++ 02.2_constant_4_part1.py
@@ -10,19 +10,19 @@ df_constant_4 = pd.read_csv("/Users/Danjing 1/Lingsu/Jobs/2024 WB STC/Sector/Dat
 
 print(df_constant_4.head(10))
 
-##########Step 2. Sector check
+#######################################################################################################################
+################################## Sector check #######################################################################
 ##### Transforme the original dataset
-######## For Constant file
 
-#Create a pivot table for the values, all sectors became column numbers
+### Create a pivot table for the values, all sectors became column numbers
 pivot_df_constant_4 = df_constant_4.pivot_table(index=['country','year','series_code','base'], columns='code', values = 'value')
 #print(pivot_df_constant_4.head(10))
 
-#Aggregate the footnote colum to check for any footnotes
+### Aggregate the footnote colum to check for any footnotes
 #print(df_constant_4['note'].dtype) #object
 footnotes = df_constant_4.groupby(['country','year','series_code','base'])['note'].agg(lambda x: ', '.join(set(y for y in x if pd.notna(y) and y != '')))
 
-#Reset the index to make 'country','year','series_code' into columns
+## Reset the index to make 'country','year','series_code' into columns
 pivot_df_constant_4.reset_index(inplace=True)
 
 # The footnotes series_code needs to have its index reset for joining. Then add it to the pivot table.
@@ -31,6 +31,7 @@ footnotes.rename(columns={'note':'Footnotes'}, inplace=True)
 pivot_df_constant_4 = pivot_df_constant_4.merge(footnotes, on=['country','year','series_code','base'], how='left')
 #print(pivot_df_constant_4.head(10))
 
+### Check for missing sectors
 # put all missing sectors into a new column
 '''Define a function to find missing sectors (columns)'''
 def missing_column(row):
@@ -54,19 +55,13 @@ pivot_df_constant_4['only_DorE_missing'] = ((pivot_df_constant_4['D_missing'] ==
 
 # print(pivot_df_constant_4.head(10))
 
-#print(pivot_df_constant_4.head(10))
-# calculate the discard score
+
+### calculate the discard score
 #columns_to_cal = [4,5]+list(range(7,20))
 pivot_df_constant_4['sum_of_sector'] = pivot_df_constant_4.iloc[:,columns_to_cal].sum(axis=1)
 pivot_df_constant_4['score'] = ((pivot_df_constant_4['sum_of_sector'] - pivot_df_constant_4['B.1g'])/pivot_df_constant_4['B.1g']).abs()
 pivot_df_constant_4['discard'] = (pivot_df_constant_4['score']-0.005)>=0
 print(pivot_df_constant_4.head(10))
-
-# if b.1g is empty, do not discard. (discard = false)
-for index in range(len(pivot_df_constant_4)):
-    if pd.isna(pivot_df_constant_4.loc[index, 'B.1g']):  # Check if the value in A is NaN (or None)
-        pivot_df_constant_4.loc[index, 'discard'] = False     # Set B to False if A is empty
-
 
 # check if footnote 7 and footnote 37 is included.
 pivot_df_constant_4['footnote_7'] = pivot_df_constant_4['Footnotes'].str.contains(r'\b7\b', na=False)
@@ -96,43 +91,60 @@ for index in range(len(pivot_df_constant_4)):
 
 #print(pivot_df_constant_4.head(10))
 
+### if b.1g is empty, do not discard. (discard = false)
+for index in range(len(pivot_df_constant_4)):
+    if pd.isna(pivot_df_constant_4.loc[index, 'B.1g']):  # Check if the value in A is NaN (or None)
+        pivot_df_constant_4.loc[index, 'discard'] = False     # Set B to False if A is empty
+
+
 # Save a copy of current discard column, without considering the number of sectors.
-pivot_df_constant_4['discard_nc_Nsector'] = pivot_df_constant_4['discard']
+pivot_df_constant_4['discard_on_score'] = pivot_df_constant_4['discard']
 
 # If the n_sector is smaller than 15 (total sector columns), unless only missing D or E, then discard. Otherwise,  do not discard.
 conditions = (pivot_df_constant_4['n_sectors'] < 15) & (pivot_df_constant_4['only_DorE_missing'] == False)
+pivot_df_constant_4['discard_on_sector'] = conditions
 pivot_df_constant_4.loc[conditions, 'discard'] = True
 
-### Final Data Clean and Export
-#Our period of interest is 1990 – 2023.
+#######################################################################################################################
+################# Final Data Clean and Export
+
+### Our period of interest is 1990 – 2023.
 pivot_df_constant_4 = pivot_df_constant_4[(pivot_df_constant_4['year']>=1990) & (pivot_df_constant_4['year']<=2023)]
 
-# reset year and series into integer
+### reset year and series into integer
 pivot_df_constant_4['year'] = pivot_df_constant_4['year'].astype(int)
 pivot_df_constant_4['series_code'] = pivot_df_constant_4['series_code'].astype(int)
 
-# Merge the iso3
-iso_mapping = pd.read_csv("/Users/Danjing 1/Lingsu/Jobs/2024 WB STC/Sector/Process/iso_mapping.csv")
-pivot_df_constant_4 = pd.merge(pivot_df_constant_4, iso_mapping, on='country', how='left')
+### Merge the iso3
+iso_mapping_un4 = pd.read_csv("/Users/Danjing 1/Lingsu/Jobs/2024 WB STC/Sector/Process/iso_mapping_un4.csv")
+pivot_df_constant_4 = pd.merge(pivot_df_constant_4, iso_mapping_un4, on='country', how='left')
 
 #export the pivot table dataframe to a csv file
 print(pivot_df_constant_4.head(10))
 pivot_df_constant_4.to_csv("/Users/Danjing 1/Lingsu/Jobs/2024 WB STC/Sector/Process/un4_constant_Sectorvalue.csv")
 
-# Keep only those not discarded.
-df_constant_4_int = pivot_df_constant_4[pivot_df_constant_4['discard']== False]
-#df_constant_4_int.drop(columns='discard', inplace=True)
-print(df_constant_4_int.head(10))
-
-df_constant_4_int.to_csv("/Users/Danjing 1/Lingsu/Jobs/2024 WB STC/Sector/Process/un4_constant_intermediate.csv")
-
 # Keep those discarded in a seperate file
 df_constant_4_int_discard = pivot_df_constant_4[pivot_df_constant_4['discard']== True]
+df_constant_4_int_discard.reset_index(inplace=True, drop=True)
+print(df_constant_4_int_discard.tail(10))
 df_constant_4_int_discard.to_csv("/Users/Danjing 1/Lingsu/Jobs/2024 WB STC/Sector/Process/un4_constant_intermediate_discard.csv")
 
+# Keep only those not discarded.
+df_constant_4_int = pivot_df_constant_4[pivot_df_constant_4['discard']== False]
+df_constant_4_int.drop(columns='discard', inplace=True)
+print(df_constant_4_int.head(10))
+
+# Keep only columns interested.
+columns_to_keep = ['country', 'iso3', 'year','series_code','Footnotes','sector_missing','score','discard_on_score', 'discard_on_sector']
+df_constant_4_int = df_constant_4_int.loc[:, columns_to_keep]
+df_constant_4_int.reset_index(inplace=True, drop=True)
+print(df_constant_4_int.tail(10))
+df_constant_4_int.to_csv("/Users/Danjing 1/Lingsu/Jobs/2024 WB STC/Sector/Process/un4_constant_intermediate.csv")
+
+
 # Export a file that only contains country, year, series_code, and iso3.
-df_constant_4_int = pd.concat([df_constant_4_int.iloc[:,0:4],df_constant_4_int.iloc[:,-1: ]], axis=1)
-#df_constant_4_int = df_constant_4_int.iloc[:,0:4]
+#df_constant_4_int = pd.concat([df_constant_4_int.iloc[:,0:4],df_constant_4_int.iloc[:,-1: ]], axis=1)
+df_constant_4_int = df_constant_4_int.iloc[:,0:4]
 print(df_constant_4_int.head())
 df_constant_4_int.to_csv("/Users/Danjing 1/Lingsu/Jobs/2024 WB STC/Sector/Process/un4_constant_4cols.csv")
 
